eeafa7e7c447ec207d064ec37ab98a69bf2990cf
Joshua Christianson <macgyverjunk@yahoo.com>
2022-05-31 11:13:59 -0500

    Updates

---
+++ precache-manifest.53489ee12314f2d3b9387f1562da21c1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837af163f05afa54b550ac914ae10d9a",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "4df4e55814f55c7caeac",
+    "url": "/ReactPortfolio/static/css/main.f16bd087.chunk.css"
+  },
+  {
+    "revision": "f6a59b9f85ca565977a6",
+    "url": "/ReactPortfolio/static/js/2.096e9bf0.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/ReactPortfolio/static/js/2.096e9bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4df4e55814f55c7caeac",
+    "url": "/ReactPortfolio/static/js/main.2020823f.chunk.js"
+  },
+  {
+    "revision": "f6ee6a042145b5e30d79",
+    "url": "/ReactPortfolio/static/js/runtime-main.c4689b6c.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/ReactPortfolio/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/ReactPortfolio/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/ReactPortfolio/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/ReactPortfolio/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/ReactPortfolio/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/ReactPortfolio/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/ReactPortfolio/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/ReactPortfolio/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/ReactPortfolio/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "2810a88b1d6365b363caeabe678f1629",
+    "url": "/ReactPortfolio/static/media/logo.2810a88b.svg"
+  },
+  {
+    "revision": "61baa2dd481e3e582c5813fe841da969",
+    "url": "/ReactPortfolio/static/media/ss_quickmovielookup.61baa2dd.png"
+  },
+  {
+    "revision": "27d0ecfd37c4446429f4cb3050f97841",
+    "url": "/ReactPortfolio/static/media/ss_techblog.27d0ecfd.png"
+  },
+  {
+    "revision": "23222a3e05392597f407bcd7e1a3beec",
+    "url": "/ReactPortfolio/static/media/ss_therecipeforsuccess.23222a3e.png"
+  },
+  {
+    "revision": "29bee180762719602766a99b1360b5b5",
+    "url": "/ReactPortfolio/static/media/video-placeholder.29bee180.jpg"
+  }
+]);
